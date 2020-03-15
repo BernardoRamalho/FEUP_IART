6e61a6b91f479a61b7de30dd84eb0b5de28274c2
BernardoRamalho <bernardoramalho99@gmail.com>
2020-03-15 19:22:17 +0000

    Saved the pieces in a dictionary

--- piece.py
+++ piece.py
@@ -5,58 +5,51 @@ class Piece:
     radius = 50
 
     def __init__(self, x_position, y_position, direction):
-        self.x = x_position
-        self.y = y_position
+        self.position = [x_position, y_position]
         self.direction = direction
         self.evolved = False
 
     def invert_direction(self):
         self.evolved = not self.evolved
 
-    def set_x(self, new_x):
-        self.x = new_x
+    def set_position(self, new_position):
+        self.position = new_position
 
-    def set_y(self, new_y):
-        self.y = new_y
-
-    def get_y(self):
-        return self.y
-
-    def get_x(self):
-        return self.x
+    def get_position(self):
+        return self.position
 
     def draw_triangle(self, screen, triangle_number):
 
         if self.direction == 'h':
             if triangle_number == 1:
-                point_1 = (self.x - self.radius, self.y)
-                point_2 = (self.x - 5, self.y - 15)
-                point_3 = (self.x - 5, self.y + 15)
+                point_1 = (self.position[0] - self.radius, self.position[1])
+                point_2 = (self.position[0] - 5, self.position[1] - 15)
+                point_3 = (self.position[0] - 5, self.position[1] + 15)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
 
             else:
-                point_1 = (self.x + self.radius, self.y)
-                point_2 = (self.x + 5, self.y - 15)
-                point_3 = (self.x + 5, self.y + 15)
+                point_1 = (self.position[0] + self.radius, self.position[1])
+                point_2 = (self.position[0] + 5, self.position[1] - 15)
+                point_3 = (self.position[0] + 5, self.position[1] + 15)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
 
         elif self.direction == 'v':
             if triangle_number == 1:
-                point_1 = (self.x, self.y - self.radius)
-                point_2 = (self.x - 15, self.y - 5)
-                point_3 = (self.x + 15, self.y - 5)
+                point_1 = (self.position[0], self.position[1] - self.radius)
+                point_2 = (self.position[0] - 15, self.position[1] - 5)
+                point_3 = (self.position[0] + 15, self.position[1] - 5)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
 
             else:
-                point_1 = (self.x, self.y + self.radius)
-                point_2 = (self.x - 15, self.y + 5)
-                point_3 = (self.x + 15, self.y + 5)
+                point_1 = (self.position[0], self.position[1] + self.radius)
+                point_2 = (self.position[0] - 15, self.position[1] + 5)
+                point_3 = (self.position[0] + 15, self.position[1] + 5)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
@@ -68,7 +61,7 @@ class Piece:
         else:
             colour = (254, 200, 66)  # yellow
 
-        pygame.draw.circle(screen, colour, (int(self.x), int(self.y)), int(self.radius))
+        pygame.draw.circle(screen, colour, (int(self.position[0]), int(self.position[1])), int(self.radius))
 
         self.draw_triangle(screen, 1)
         self.draw_triangle(screen, 2)
--- pivit.py
+++ pivit.py
@@ -1,5 +1,13 @@
 import pygame
 from piece import Piece
+from collections import defaultdict
+
+
+# Basic Information About Structure the Game
+# 1º- The Pieces are saved in a dictionary which the key is a tuple containing the position of the piece and the value
+# is the key itself;
+# 2º- You can exit the Game by pressing the key q;
+# 3º- I already made a function to change the position of a key but haven't implemented the event to do so.
 
 
 #
@@ -10,50 +18,41 @@ from piece import Piece
 
 def create_pieces(player, player_pieces, square_side_size):
     if player == 2:
-        player_pieces.append(Piece(square_side_size / 2, 1 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(Piece(square_side_size / 2, 3 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(Piece(square_side_size / 2, 4 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(Piece(square_side_size / 2, 6 * square_side_size + square_side_size / 2, 'h'))
 
-        player_pieces.append(
-            Piece(7 * square_side_size + square_side_size / 2, 1 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(
-            Piece(7 * square_side_size + square_side_size / 2, 3 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(
-            Piece(7 * square_side_size + square_side_size / 2, 4 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(
-            Piece(7 * square_side_size + square_side_size / 2, 6 * square_side_size + square_side_size / 2, 'h'))
+        for i in range(1, 7):
+            if i == 2 or i == 5:
+                piece = Piece(i * square_side_size + square_side_size / 2, square_side_size / 2, 'v')
+                player_pieces[tuple(piece.get_position())] = piece
 
-        player_pieces.append(Piece(2 * square_side_size + square_side_size / 2, square_side_size / 2, 'v'))
-        player_pieces.append(Piece(5 * square_side_size + square_side_size / 2, square_side_size / 2, 'v'))
+                piece = Piece(i * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2,
+                              'v')
+                player_pieces[tuple(piece.get_position())] = piece
 
-        player_pieces.append(
-            Piece(2 * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2, 'v'))
-        player_pieces.append(
-            Piece(5 * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2, 'v'))
+            else:
+                piece = Piece(square_side_size / 2, i * square_side_size + square_side_size / 2, 'h')
+                player_pieces[tuple(piece.get_position())] = piece
 
+                piece = Piece(7 * square_side_size + square_side_size / 2, i * square_side_size + square_side_size / 2,
+                              'h')
+                player_pieces[tuple(piece.get_position())] = piece
     else:
-        player_pieces.append(Piece(1 * square_side_size + square_side_size / 2, square_side_size / 2, 'v'))
-        player_pieces.append(Piece(3 * square_side_size + square_side_size / 2, square_side_size / 2, 'v'))
-        player_pieces.append(Piece(4 * square_side_size + square_side_size / 2, square_side_size / 2, 'v'))
-        player_pieces.append(Piece(6 * square_side_size + square_side_size / 2, square_side_size / 2, 'v'))
 
-        player_pieces.append(
-            Piece(1 * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2, 'v'))
-        player_pieces.append(
-            Piece(3 * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2, 'v'))
-        player_pieces.append(
-            Piece(4 * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2, 'v'))
-        player_pieces.append(
-            Piece(6 * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2, 'v'))
+        for i in range(1, 7):
+            if i == 2 or i == 5:
+                piece = Piece(square_side_size / 2, i * square_side_size + square_side_size / 2, 'h')
+                player_pieces[tuple(piece.get_position())] = piece
 
-        player_pieces.append(Piece(square_side_size / 2, 2 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(Piece(square_side_size / 2, 5 * square_side_size + square_side_size / 2, 'h'))
+                piece = Piece(7 * square_side_size + square_side_size / 2, i * square_side_size + square_side_size / 2,
+                              'h')
+                player_pieces[tuple(piece.get_position())] = piece
 
-        player_pieces.append(
-            Piece(7 * square_side_size + square_side_size / 2, 2 * square_side_size + square_side_size / 2, 'h'))
-        player_pieces.append(
-            Piece(7 * square_side_size + square_side_size / 2, 5 * square_side_size + square_side_size / 2, 'h'))
+            else:
+                piece = Piece(i * square_side_size + square_side_size / 2, square_side_size / 2, 'v')
+                player_pieces[tuple(piece.get_position())] = piece
+
+                piece = Piece(i * square_side_size + square_side_size / 2, 7 * square_side_size + square_side_size / 2,
+                              'v')
+                player_pieces[tuple(piece.get_position())] = piece
 
 
 def draw_board(screen, square_side, screen_size):
@@ -77,20 +76,29 @@ def draw_board(screen, square_side, screen_size):
 
 
 def draw_pieces(screen, pieces):
-    for i in pieces[0]:
+    for i in pieces[0].values():
         i.draw(screen, 1)
 
-    for i in pieces[1]:
+    for i in pieces[1].values():
         i.draw(screen, 2)
+
+
+def change_piece_position(pieces, piece, new_position):
+    del pieces[tuple(piece.position)]
+    piece.set_position(new_position)
+    pieces[tuple(new_position)] = piece
 
 
 def event_handler(variables):
     for event in pygame.event.get():
 
+        # Key pressed
         if event.type == pygame.KEYDOWN:
+
             if event.key == pygame.K_q:
                 variables[0] = False
 
+        # Closed Window
         if event.type == pygame.QUIT:
             variables[0] = False
 
@@ -100,8 +108,8 @@ def main():
 
     # Variable Initiation
     screen_size = 1200
-    player1_pieces = []  # List containing all the pieces of player one
-    player2_pieces = []  # List containing all the pieces of player two
+    player1_pieces = defaultdict(tuple)  # List containing all the pieces of player one
+    player2_pieces = defaultdict(tuple)  # List containing all the pieces of player two
     screen = pygame.display.set_mode((screen_size, screen_size))  # Where the game is shown
     square_side = screen_size / 8  # 8 because the board is 8x8
     run = True
