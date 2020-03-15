dfb7970472b4391e31d68fb909906c827ffa3f28
BernardoRamalho <bernardoramalho99@gmail.com>
2020-03-15 16:56:03 +0000

    Created Pieces Class

---
+++ piece.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Piece:
+
+    radius = 50
+
+    def __init__(self, x_position, y_position, direction):
+        self.x = x_position
+        self.y = y_position
+        self.direction = direction
+        self.evolved = False
+
+    def invert_direction(self):
+        self.evolved = not self.evolved
+
+    def set_x(self, new_x):
+        self.x = new_x
+
+    def set_y(self, new_y):
+        self.y = new_y
+
+    def get_y(self):
+        return self.y
+
+    def get_x(self):
+        return self.x
+
+    def draw(self, screen, player, square_side):
+
+        if player == 1:
+            colour = (66, 135, 245)  # blue
+        else:
+            colour = (254, 200, 66)  # yellow
+
+        pygame.draw.circle(screen, colour, (int(self.x + square_side / 2), int(self.y + square_side / 2)), self.radius)
--- pivit.py
+++ pivit.py
@@ -1,21 +1,67 @@
 import pygame
+from piece import Piece
+
+
+def create_pieces(player, player_pieces, square_side_size):
+    if player == 2:
+        player_pieces.append(Piece(0, 1 * square_side_size, 'v'))
+        player_pieces.append(Piece(0, 3 * square_side_size, 'v'))
+        player_pieces.append(Piece(0, 4 * square_side_size, 'v'))
+        player_pieces.append(Piece(0, 6 * square_side_size, 'v'))
+
+        player_pieces.append(Piece(7 * square_side_size, 1 * square_side_size, 'v'))
+        player_pieces.append(Piece(7 * square_side_size, 3 * square_side_size, 'v'))
+        player_pieces.append(Piece(7 * square_side_size, 4 * square_side_size, 'v'))
+        player_pieces.append(Piece(7 * square_side_size, 6 * square_side_size, 'v'))
+
+        player_pieces.append(Piece(2 * square_side_size, 0, 'h'))
+        player_pieces.append(Piece(5 * square_side_size, 0, 'h'))
+
+        player_pieces.append(Piece(2 * square_side_size, 7 * square_side_size, 'h'))
+        player_pieces.append(Piece(5 * square_side_size, 7 * square_side_size, 'h'))
+
+    else:
+        player_pieces.append(Piece(1 * square_side_size, 0, 'h'))
+        player_pieces.append(Piece(3 * square_side_size, 0, 'h'))
+        player_pieces.append(Piece(4 * square_side_size, 0, 'h'))
+        player_pieces.append(Piece(6 * square_side_size, 0, 'h'))
+
+        player_pieces.append(Piece(1 * square_side_size, 7, 'h'))
+        player_pieces.append(Piece(3 * square_side_size, 7, 'h'))
+        player_pieces.append(Piece(4 * square_side_size, 7, 'h'))
+        player_pieces.append(Piece(6 * square_side_size, 7, 'h'))
+
+        player_pieces.append(Piece(0, 2 * square_side_size, 'v'))
+        player_pieces.append(Piece(0, 5 * square_side_size, 'v'))
+
+        player_pieces.append(Piece(7 * square_side_size, 2 * square_side_size, 'v'))
+        player_pieces.append(Piece(7 * square_side_size, 5 * square_side_size, 'v'))
+
 
 pygame.init()
 
-screen_size = 600
+screen_size = 1200
 
 screen = pygame.display.set_mode((screen_size, screen_size))
 
 run = True
 paint = False
 
-square_side = screen_size / 6
+player1_pieces = []
+player2_pieces = []
+
+square_side = screen_size / 8
+
+create_pieces(1, player1_pieces, square_side)
+
+create_pieces(2, player2_pieces, square_side)
+
 
 while run:
     pygame.time.delay(100)
 
     x = 0
-    y = 0   
+    y = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,30 +69,32 @@ while run:
 
     screen.fill((0, 0, 0))
 
-    for i in range(36):
+    for i in range(64):
 
-        if (y == 0 or x == 0 or y == screen_size - square_side or x == screen_size - square_side) and (y != 0 and x != 0  y == screen_size - square_side and x == screen_size - square_side) :
-            pygame.draw.circle(screen, (255, 207, 0), (int(x + 50), int(y + 50)), 40)
-
-        if(paint):
+        if paint:
             paint = False
             pygame.draw.rect(screen, (255, 255, 255), (x, y, square_side, square_side))
         else:
             paint = True
 
-        
         x += square_side
-        
-        if (x > screen_size - square_side):
+
+        if x > screen_size - square_side:
             y += square_side
-            
+
             x = 0
-            
-            if(paint):
+
+            if paint:
                 paint = False
             else:
                 paint = True
 
-        
+    for i in player1_pieces:
+        i.draw(screen, 1, square_side)
+
+    for i in player2_pieces:
+        i.draw(screen, 2, square_side)
+
     pygame.display.update()
+
 pygame.quit()
