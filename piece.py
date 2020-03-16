d61b7825f36e82ae44bcea11417c0614992af799
BernardoRamalho <bernardoramalho99@gmail.com>
2020-03-16 13:22:26 +0000

    Screen Size now depends on screen resolution and position is now a tuple

--- piece.py
+++ piece.py
@@ -2,15 +2,21 @@ import pygame
 
 
 class Piece:
-    radius = 25
 
-    def __init__(self, x_position, y_position, direction):
-        self.position = [x_position, y_position]
+    def __init__(self, x_position, y_position, direction, radius):
+        self.position = (x_position, y_position)
         self.direction = direction
         self.evolved = False
+        self.radius = radius
 
     def invert_direction(self):
-        self.evolved = not self.evolved
+        if self.direction == 'v':
+            self.direction == 'h'
+        else:
+            self.direction == 'v'
+
+    def evolve(self):
+        self.evolved = True
 
     def set_position(self, new_position):
         self.position = new_position
@@ -20,19 +26,22 @@ class Piece:
 
     def draw_triangle(self, screen, triangle_number):
 
+        space_between_triangles = self.radius * 0.1
+        half_triangle_width = self.radius * 0.2
+
         if self.direction == 'h':
             if triangle_number == 1:
                 point_1 = (self.position[0] - self.radius, self.position[1])
-                point_2 = (self.position[0] - 2, self.position[1] - 7)
-                point_3 = (self.position[0] - 2, self.position[1] + 7)
+                point_2 = (self.position[0] - space_between_triangles, self.position[1] - half_triangle_width)
+                point_3 = (self.position[0] - space_between_triangles, self.position[1] + half_triangle_width)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
 
             else:
                 point_1 = (self.position[0] + self.radius, self.position[1])
-                point_2 = (self.position[0] + 2, self.position[1] - 7)
-                point_3 = (self.position[0] + 2, self.position[1] + 7)
+                point_2 = (self.position[0] + space_between_triangles, self.position[1] - half_triangle_width)
+                point_3 = (self.position[0] + space_between_triangles, self.position[1] + half_triangle_width)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
@@ -40,16 +49,16 @@ class Piece:
         elif self.direction == 'v':
             if triangle_number == 1:
                 point_1 = (self.position[0], self.position[1] - self.radius)
-                point_2 = (self.position[0] - 7, self.position[1] - 2)
-                point_3 = (self.position[0] + 7, self.position[1] - 2)
+                point_2 = (self.position[0] - half_triangle_width, self.position[1] - space_between_triangles)
+                point_3 = (self.position[0] + half_triangle_width, self.position[1] - space_between_triangles)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
 
             else:
                 point_1 = (self.position[0], self.position[1] + self.radius)
-                point_2 = (self.position[0] - 7, self.position[1] + 2)
-                point_3 = (self.position[0] + 7, self.position[1] + 2)
+                point_2 = (self.position[0] - half_triangle_width, self.position[1] + space_between_triangles)
+                point_3 = (self.position[0] + half_triangle_width, self.position[1] + space_between_triangles)
 
                 points = [point_1, point_2, point_3]
                 pygame.draw.polygon(screen, (0, 0, 0), points)
