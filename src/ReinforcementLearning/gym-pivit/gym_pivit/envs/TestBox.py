2baea3ad960444a0032e70ac3150d3dba03a53c0
pedrocarmopereira <pppereira3@gmail.com>
2020-05-25 16:56:37 +0100

    added player turn & step now executes the action

--- src/ReinforcementLearning/gym-pivit/gym_pivit/envs/TestBox.py
+++ src/ReinforcementLearning/gym-pivit/gym_pivit/envs/TestBox.py
@@ -6,18 +6,18 @@ from pivit_env import PivitEnv
 env = PivitEnv()
 env.setup()
 
-#env.render()
-#time.sleep(4)
+env.render()
+time.sleep(4)
 
-#move = {'piece_id': 1,
-#        'new_pos': (2, 2)
-#        }
+move = {'piece_id': 1,
+        'new_pos': (2, 2)
+        }
 
-#action = env.move_to_action(move)
-env.step(-1)
-env.player_move(action, 1)
+action = env.move_to_action(move)
+env.step(action)
+#env.player_move(action, 1)
 
-#env.render()
-#time.sleep(4)
+env.render()
+time.sleep(4)
 
 
