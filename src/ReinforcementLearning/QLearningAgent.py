c3e55ae0d68c1cf12603944a84563ce1fa0328d4
pedrocarmopereira <pppereira3@gmail.com>
2020-05-26 01:29:06 +0100

    Added functions to read and write qtable to save progress

--- src/ReinforcementLearning/QLearningAgent.py
+++ src/ReinforcementLearning/QLearningAgent.py
@@ -2,6 +2,23 @@ import numpy as np
 import gym
 import gym_pivit
 import time
+import json
+
+class QLAgent:
+
+	def __init__(self, file_path):
+		self.file_path = file_path
+		self.q_table = {}
+		if file_path != "":
+			self.read_qtable()
+
+	def write_qtable(self):
+		with open(self.file_path, 'w') as file:
+			file.write(json.dumps(self.q_table))
+
+	def read_qtable(self):
+		with open(self.file_path, 'r') as file:
+			self.q_table = json.load(file)
 
 env = gym.make("pivit-v0")
 
@@ -21,10 +38,8 @@ env.reset()
 # env.render()
 
 test_str = env.state_to_string()
-q_table = {}
+# = {'A':65, 'B':66, 'C':67}
+ql_agent = QLAgent("qtable.json")
+
+print(ql_agent.q_table)
 #print(q_table)
-
-#time.sleep(4)
-
-print(test_str)
-print(hash(test_str))
